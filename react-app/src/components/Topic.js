556db468694ceb40807b65ca11975c9cf661f1da
Yu Ra Kim <yurakim.designs@gmail.com>
2022-07-01 10:18:25 -0400

    Topic navigation

--- react-app/src/components/Topic.js
+++ react-app/src/components/Topic.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { loadAllTopics } from '../store/topics';
 
@@ -18,10 +18,16 @@ const Topic = () => {
         return Object.values(state.topics)
     });
     let currentTopic;
+    let prevTopic = undefined;
+    let nextTopic = undefined;
     topics.forEach((topic) => {
         if (topic.id == topicId) {
             currentTopic = topic;
-        };
+        } else if (topic.id == Number(topicId) - 1) {
+            prevTopic = topic;
+        } else if (topic.id = Number(topicId) + 1) {
+            nextTopic = topic;
+        }
     });
 
     useEffect(() => {
@@ -29,11 +35,29 @@ const Topic = () => {
     }, []);
 
     return (
-        <div id='topic-view'>
-            {currentTopic?.title}
-            {currentTopic?.time_estimate}
-            {currentTopic?.description}
-        </div>
+        <>
+            <div id='topic-view'>
+                {currentTopic?.title}
+                {currentTopic?.time_estimate}
+                {currentTopic?.description}
+            </div>
+            <div id='topic-navigation'>
+                <div>
+                    {prevTopic ?
+                        <>
+                            <p>Previous Topic:</p>
+                            <Link to={`/topics/${prevTopic.id}`}>{prevTopic.title}</Link>
+                        </> : ''}
+                </div>
+                <div>
+                    {nextTopic ?
+                        <>
+                            <p>Next Topic:</p>
+                            <Link to={`/topics/${nextTopic.id}`}>{nextTopic.title}</Link>
+                        </> : ''}
+                </div>
+            </div>
+        </>
     )
 };
 
